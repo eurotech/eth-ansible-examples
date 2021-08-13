c2ab91a8ff7804930342b9cd0cc57d13c28928c9
Abe Shmookler <abe.shmookler@eurotech.com>
2021-08-13 13:52:06 -0700

    update .gitignore and remove generated folder /out
    Signed-off-by: Abe Shmookler <abe.shmookler@eurotech.com>

--- playbook-lora-all-in-one-gateway-basic-install/out/package/roles/post_esf_linux_setup/files/postgres_init/001-init-chirpstack_ns.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "postgres" <<-EOSQL
-    create role chirpstack_ns with login password 'CNSpwLoRa';
-    create database chirpstack_ns with owner chirpstack_ns;
-EOSQL
