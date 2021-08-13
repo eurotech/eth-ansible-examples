72c70150635f292168c7ecff7b51d5141ab3b4ad
Abe Shmookler <abe.shmookler@eurotech.com>
2021-08-13 12:39:49 -0700

    Add resetting enviornment
    Signed-off-by: Abe Shmookler <abe.shmookler@eurotech.com>

--- playbook-lora-all-in-one-gateway-basic-install/freshrun.sh
+++ playbook-lora-all-in-one-gateway-basic-install/freshrun.sh
@@ -10,21 +10,21 @@ echo ""
 echo "~~~~~~Extract New Files~~~~~~"
 tar -xvf lora-all-in-one-gateway-base-install-0.1.0-playbook.tar.gz --one-top-level=ansible
 
-echo ""
-echo "~~~~~~Stop All Docker Containers~~~~~~"
-docker kill $(docker ps -q)
+# echo ""
+# echo "~~~~~~Stop All Docker Containers~~~~~~"
+# docker kill $(docker ps -q)
 
-echo ""
-echo "~~~~~~Delete All Docker Containers~~~~~~"
-docker rm $(docker ps -a -q)
+# echo ""
+# echo "~~~~~~Delete All Docker Containers~~~~~~"
+# docker rm $(docker ps -a -q)
 
-echo ""
-echo "~~~~~~Delete All Docker Volumes~~~~~~"
-docker volume rm $(docker volume ls -q)
+# echo ""
+# echo "~~~~~~Delete All Docker Volumes~~~~~~"
+# docker volume rm $(docker volume ls -q)
 
-echo ""
-echo "~~~~~~Delete Chirpbridge Docker Network~~~~~~"
-docker network rm chirpbridge
+# echo ""
+# echo "~~~~~~Delete Chirpbridge Docker Network~~~~~~"
+# docker network rm chirpbridge
 
 echo ""
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
