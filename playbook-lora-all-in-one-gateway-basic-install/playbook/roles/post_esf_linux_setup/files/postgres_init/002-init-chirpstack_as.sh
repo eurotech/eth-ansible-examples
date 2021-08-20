c960a8655e4b0d6445e813e1d163a9be4f004e54
Abe Shmookler <abe.shmookler@eurotech.com>
2021-08-20 14:42:15 -0700

    remove files now in AWS
    Signed-off-by: Abe Shmookler <abe.shmookler@eurotech.com>

--- playbook-lora-all-in-one-gateway-basic-install/playbook/roles/post_esf_linux_setup/files/postgres_init/002-init-chirpstack_as.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "postgres" <<-EOSQL
-    create role chirpstack_as with login password 'CASpwLoRa';
-    create database chirpstack_as with owner chirpstack_as;
-EOSQL
